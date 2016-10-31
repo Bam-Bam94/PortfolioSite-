e87b794407b6902e0041f02775e7c175c2801862
mike <mikeclbrs@gmail.com>
2016-10-30 23:31:34 -0400

    finished Project box and skill box

--- indexScript.js
+++ indexScript.js
@@ -18,4 +18,19 @@ function Back() {
 
   whiteBackground.style.display = "none";
   skillBoxAlert.style.display = "none";
+  ProjectBoxAlert.style.display = "none";
+}
+
+function showProjects() {
+  var whiteBackground = document.getElementById('white-background');
+  var skillBoxAlert = document.getElementById('ProjectBoxAlert');
+
+  whiteBackground.style.display = "block";
+  skillBoxAlert.style.display = "block";
+
+  var winWidth = window.innerWidth;
+  var winHeight = window.innerHeight;
+
+  skillBoxAlert.style.left = (winWidth/2) - 480/2 + "px";
+  skillBoxAlert.style.top = "150px";
 }
