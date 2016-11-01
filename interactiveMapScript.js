3453d984a87cd9cb798e9af98d8f4cbe9a808577
mike <mikeclbrs@gmail.com>
2016-11-01 16:00:19 -0400

    deleted things I did not need and added in google map

---
+++ interactiveMapScript.js
@@ -0,0 +1,5 @@
+ function initMap() {
+   var pittsburgh = {lat: 40.44062479999999, lng: -79.99588640000002};
+   var map = new google.maps.Map(document.getElementById('map'), {zoom: 11, center: pittsburgh});
+   var marker = new google.maps.Marker({position: pittsburgh, map: map});
+};
