35922f58b7d9c73731df310c5059ffc0568a9603
mike <mikeclbrs@gmail.com>
2016-11-02 16:52:38 -0400

    some deleted files

--- interactiveMapScript.js
+++ interactiveMapScript.js
@@ -1,5 +1,8 @@
+
+
  function initMap() {
    var pittsburgh = {lat: 40.44062479999999, lng: -79.99588640000002};
    var map = new google.maps.Map(document.getElementById('map'), {zoom: 11, center: pittsburgh});
    var marker = new google.maps.Marker({position: pittsburgh, map: map});
+   displayMap();
 };
