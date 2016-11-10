ecfcb9e5e93913426a2414f39025d447602fb9e7
mike <mikeclbrs@gmail.com>
2016-11-10 17:09:54 -0500

    Having troubles with onclick

--- applications/Map/mapAppScript.js
+++ applications/Map/mapAppScript.js
@@ -1,8 +1,8 @@
 var model = {
   slide: [1,2,3,4,5],
-  slidePostion : 0;
+  slidePostion : 0,
   slidePostionMover: function(direction){
-                for ( this.slide[this.slidePostion]){
+                  while ( this.slide[this.slidePostion]){
                   if (direction = "right"){
                     //fix bug if at an end of the array
                     this.slidePostion++;
@@ -19,10 +19,26 @@ var model = {
 };
 
 var view = {
-    position: model.slide,
-    slideText: function (position){
-          if (this.position){
+    displayMessage : function(msg){
+      var messageArea = document.getElementById("text")
+      messageArea = msg;
 
+    },
+
+    position: model.slidePostion,
+    slideText: function() {
+          if (this.position = 0){
+              this.displayMessage("1");
+          }else if (this.position = 1){
+              this.displayMessage("2");
+          }else if (this.position = 2){
+              this.displayMessage("3");
+          }else if (this.position = 3){
+              this.displayMessage("4");
+          }else if (this.position = 4){
+              this.displayMessage("5");
+          }else if (this.position = 5){
+              this.displayMessage("6");
           }
 
     },
@@ -31,6 +47,26 @@ var view = {
 
 var controller = {
 
+    ProcessRequest: function(side){
+        if (side="right"){
+          model.slidePostionMover("right");
+        }
+        else if (side="left") {
+          model.slidePostionMover("left");
+        }
+        view.slideText;
+        //view.updateMap;
+    },
 
+
+};
+
+function handleRightArrow(){
+  controller.ProcessRequest("right");
+
+};
+
+function handleLeftArrow(){
+  controller.ProcessRequest("left");
 
 };
