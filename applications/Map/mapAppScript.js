cc50c6d08ae889ca287bd1e65ba37f5a9e02a8da
mike <mikeclbrs@gmail.com>
2016-11-10 02:05:16 -0500

    started work on JS for map

--- applications/Map/mapAppScript.js
+++ applications/Map/mapAppScript.js
@@ -1,16 +1,36 @@
 var model = {
-  slide: [],
+  slide: [1,2,3,4,5],
+  slidePostion : 0;
+  slidePostionMover: function(direction){
+                for ( this.slide[this.slidePostion]){
+                  if (direction = "right"){
+                    //fix bug if at an end of the array
+                    this.slidePostion++;
+                  } else if (direction = "left") {
+                      //fix bug if at an end of the array
+                      this.slidePostion--;
+                  }
+                };
 
 
-}
+  },
+
+
+};
 
 var view = {
+    position: model.slide,
+    slideText: function (position){
+          if (this.position){
 
+          }
 
-}
+    },
+
+};
 
 var controller = {
-    var ="hello";
 
 
-}
+
+};
