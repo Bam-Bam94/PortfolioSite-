1eb2bac698c20b4194f411a2f0fe9d2469317266
mike <mikeclbrs@gmail.com>
2016-11-12 08:57:27 -0500

    got arrows to work

--- applications/Map/mapAppScript.js
+++ applications/Map/mapAppScript.js
@@ -18,11 +18,11 @@ var model = {
                   if (direction == "right"){
                     model.slidePostion = model.slidePostion + 1;
                     console.log(this.slidePostion);
-                    view.slideText;
+
 
                   } else if (direction == "left") {
-                      //fix bug if at an end of the array
-                      this.slidePostion - 1;
+                      model.slidePostion = model.slidePostion - 1;
+
                   }
                 },
 
@@ -41,17 +41,17 @@ var view = {
 
     position: model.slidePostion,
     slideText: function() {
-          if (model.position == 0){
+          if (model.slidePostion == 0){
               this.displayMessage("1");
-          }else if (model.position == 1){
+          }else if (model.slidePostion == 1){
               this.displayMessage("2");
-          }else if (model.position == 2){
+          }else if (model.slidePostion == 2){
               this.displayMessage("3");
-          }else if (model.position == 3){
+          }else if (model.slidePostion == 3){
               this.displayMessage("4");
-          }else if (model.position == 4){
+          }else if (model.slidePostion == 4){
               this.displayMessage("5");
-          }else if (model.position == 5){
+          }else if (model.slidePostion == 5){
               this.displayMessage("6");
           }
 
@@ -68,7 +68,7 @@ var controller = {
         else if (side=="left") {
           model.slidePostionMover("left");
         }
-        view.slideText;
+        view.slideText();
         //view.updateMap;
     },
 
