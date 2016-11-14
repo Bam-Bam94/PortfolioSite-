ab77eb808def410a048aa8bc931454fdb3851679
mike <mikeclbrs@gmail.com>
2016-11-14 17:01:43 -0500

    got text link issue fixed need to add more style and markers

--- applications/Map/interactiveMapScript.js
+++ applications/Map/interactiveMapScript.js
@@ -1,30 +1,4 @@
 
-/*function displayMap(){
-  var mapDisplay =document.getElementById('map');
-
-  mapDisplay.style.display = "block";
-
-  var winWidth = window.innerWidth;
-  var winHeight = window.innerHeight;
-
-  mapDisplay.style.left = (winWidth/2) - 300 + "px";
-  mapDisplay.style.top =  (winHeight/2) - 480/2 + "px";
-}
-
-
-/*function displayDialogBox(){
-  var DialogBox =document.getElementById('dialogBox');
-
-  dialogBox.style.display = "block";
-
-  var winWidth = window.innerWidth;
-  var winHeight = window.innerHeight;
-
-  dialogBox.style.left = (winWidth/2) - 300 + "px";
-  dialogBox.style.top =  (winHeight/2) - 480/2 +  "px";
-
-  dialogBox.style.height = (winHeight/2) - 300 + "px";
-}*/
 
 
  function initMap() {
@@ -32,6 +6,5 @@
    var map = new google.maps.Map(document.getElementById('map'), {zoom: 11, center: pittsburgh});
    var marker = new google.maps.Marker({position: pittsburgh, map: map});
 
-  // displayMap();
-   //displayDialogBox();
+  
 }
--- applications/Map/mapAppScript.js
+++ applications/Map/mapAppScript.js
@@ -46,13 +46,13 @@ var view = {
           }else if (model.slidePostion == 1){
               this.displayMessage("<p>Above is ariel view of the city of Pittsburgh which is my hometown. I highlighted important landmarks and places that were important to me while growing up. Each position will be marked on the map as you click through the slides. Each marker is clickable and will have more information about the marker.</p>");
           }else if (model.slidePostion == 2){
-              this.displayMessage("<p></p>");
+              this.displayMessage("<p>First, let us look at some the famous neighborhood and landmarks of the city of Pittsburgh that have had a large impact on my life. Feel free to examine the areas by clicking the markers on the map!</p>");
           }else if (model.slidePostion == 3){
-              this.displayMessage("<p></p>");
+              this.displayMessage("<p>Next, let us look at the neighborhoods above and look at specific places I found to be important locations for my life.</p>");
           }else if (model.slidePostion == 4){
-              this.displayMessage("<p></p>");
+              this.displayMessage("<p>Here we have these same locations but with the addition of the neighborhoods and landmarks added back in. </p>");
           }else if (model.slidePostion == 5){
-              this.displayMessage("Feel free to use the map above to explore around the city ");
+              this.displayMessage('"<p>Feel free to use the map above to explore around the city! If you have any questions feel free to reach out to me using the </p> <a href ="../../contact.html">contact form</a>');
           }
 
     },
