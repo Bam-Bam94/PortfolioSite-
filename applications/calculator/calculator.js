4cea2acb824558b5fec5133bd1e666c2f6ae0529
mike <mikeclbrs@gmail.com>
2016-11-25 23:31:50 -0500

    added some to js calc

---
+++ applications/calculator/calculator.js
@@ -0,0 +1,35 @@
+var model =  {
+    add : function (a,b) {
+        return a + b;
+
+    }
+    minus : function (a,b){
+        return a - b;
+
+    }
+    multiply : function (a,b){
+      return a * b;
+    }
+    division (a,b){
+      return a / b;
+    }
+
+
+
+
+};
+
+
+
+var controller = {
+
+
+};
+
+
+var view = {
+
+
+
+
+};
