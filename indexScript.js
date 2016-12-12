0be560871972188abba1f518bc6b0a88319b6a3e
mike <mikeclbrs@gmail.com>
2016-12-12 15:22:26 -0500

    made code pretty

--- indexScript.js
+++ indexScript.js
@@ -1,36 +1,36 @@
 function showSkills() {
-  var whiteBackground = document.getElementById('white-background');
-  var skillBoxAlert = document.getElementById('skillBoxAlert');
+    var whiteBackground = document.getElementById('white-background');
+    var skillBoxAlert = document.getElementById('skillBoxAlert');
 
-  whiteBackground.style.display = "block";
-  skillBoxAlert.style.display = "block";
+    whiteBackground.style.display = "block";
+    skillBoxAlert.style.display = "block";
 
-  var winWidth = window.innerWidth;
-  var winHeight = window.innerHeight;
+    var winWidth = window.innerWidth;
+    var winHeight = window.innerHeight;
 
-  skillBoxAlert.style.left = (winWidth/2) - 480/2 + "px";
-  skillBoxAlert.style.top =  (winHeight/2) - 480/2 + "px";
+    skillBoxAlert.style.left = (winWidth / 2) - 480 / 2 + "px";
+    skillBoxAlert.style.top = (winHeight / 2) - 480 / 2 + "px";
 }
 
 function Back() {
-  var whiteBackground = document.getElementById('white-background');
-  var skillBoxAlert = document.getElementById('skillBoxAlert');
+    var whiteBackground = document.getElementById('white-background');
+    var skillBoxAlert = document.getElementById('skillBoxAlert');
 
-  whiteBackground.style.display = "none";
-  skillBoxAlert.style.display = "none";
-  ProjectBoxAlert.style.display = "none";
+    whiteBackground.style.display = "none";
+    skillBoxAlert.style.display = "none";
+    ProjectBoxAlert.style.display = "none";
 }
 
 function showProjects() {
-  var whiteBackground = document.getElementById('white-background');
-  var skillBoxAlert = document.getElementById('ProjectBoxAlert');
+    var whiteBackground = document.getElementById('white-background');
+    var skillBoxAlert = document.getElementById('ProjectBoxAlert');
 
-  whiteBackground.style.display = "block";
-  skillBoxAlert.style.display = "block";
+    whiteBackground.style.display = "block";
+    skillBoxAlert.style.display = "block";
 
-  var winWidth = window.innerWidth;
-  var winHeight = window.innerHeight;
+    var winWidth = window.innerWidth;
+    var winHeight = window.innerHeight;
 
-  skillBoxAlert.style.left = (winWidth/2) - 480/2 + "px";
-  skillBoxAlert.style.top = (winHeight/2) - 480/2 + "px";
+    skillBoxAlert.style.left = (winWidth / 2) - 480 / 2 + "px";
+    skillBoxAlert.style.top = (winHeight / 2) - 480 / 2 + "px";
 }
