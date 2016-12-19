ca39bcb6103e155d8c27dfd0036247c32d9a94f4
mike <mikeclbrs@gmail.com>
2016-12-19 17:07:13 -0500

    dsfdsa

--- test2.js
+++ test2.js
@@ -14,7 +14,7 @@ app.listen(port);
 console.log("now listing to port", port);
 
 
-app.post("/contact.html", function(req,res){
+app.post("/", function(req,res){
   var api_key = 'key-be27d45d6aa9daf33f64493e112008ca';
 var domain = 'sandboxbd32b566d95344fb935cd060e31ffd99.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
