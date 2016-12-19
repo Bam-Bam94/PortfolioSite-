8aba5280098e0b16de3746fb0c6546b6206cb19f
mike <mikeclbrs@gmail.com>
2016-12-19 16:01:22 -0500

    added mailgun

--- test2.js
+++ test2.js
@@ -12,3 +12,24 @@ app.get('/', function(req,res){
 var port = 80;
 app.listen(port);
 console.log("now listing to port", port);
+
+
+app.post("/contact.html", function(req,res){
+  var api_key = 'key-be27d45d6aa9daf33f64493e112008ca';
+var domain = 'sandboxbd32b566d95344fb935cd060e31ffd99.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+var data = {
+  from: 'Mailgun <postmaster@sandboxbd32b566d95344fb935cd060e31ffd99.mailgun.org>',
+  to: 'mikeclbrs@gmail.com',
+  subject: 'Hello',
+  text: 'Name:'+ req.body.name </br>
+        'Email: '+ req.body.email </br>
+        'Phonenumber: ' + req.body.phonenumber </br>
+        'Message: ' + req.body.message </br>
+};
+
+mailgun.messages().send(data, function (error, body) {
+  console.log(body);
+});
+})
