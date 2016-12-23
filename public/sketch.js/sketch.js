3c443e282954af6e9b1276786d9e11a2f243ee83
mike <mikeclbrs@gmail.com>
2016-12-23 14:08:29 -0500

    made p5.js sketch

---
+++ public/sketch.js/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(window.innerWidth,window.innerHeight)
+}
+
+function draw() {
+  background(200,200,200,100);
+  ellipse(window.width/2,window.height/2,250,250)
+  strokeWeight(4);
+}
