a5ee77246cd1986469022fccbe4ac33a76fa91cf
mike <mikeclbrs@gmail.com>
2016-12-28 19:52:37 -0500

    MC is complete logo

--- public/canvas/marker.js
+++ public/canvas/marker.js
@@ -1,35 +1,39 @@
-function marker(x,y,xmove,ymove,ymax,ymin) {
-  this.x = x;
-  this.y = y;
-  
-  //this.v = createVector(this.x,this.y); 
-  
-  this.history = [];
-  
-  
-  //stop here 
-  this.update = function(){
-    if (this.y >=ymin  && this.y <= ymax){
-    this.x = this.x + xmove;
-    this.y = this.y - ymove;
+function marker(x, y, xmove, ymove, ymax, ymin,ymult) {
+    this.x = x;
+    this.y = y;
+    this.ymulti = 1;
+
+
+    //this.v = createVector(this.x,this.y);
+
+    this.history = [];
+
+
+    //stop here
+    this.update = function() {
+        if (this.y >= ymin && this.y <= ymax) {
+            this.x = this.x + xmove;
+            this.ymulti = this.ymulti*ymult;
+            this.y = this.y - ymove*this.ymulti;
+
+        }
+
+
+        this.v = createVector(this.x, this.y);
+        this.history.push(this.v);
+
     }
-    
-    
-    this.v = createVector(this.x,this.y);
-    this.history.push(this.v);
-    console.log(this.history);
-  }
-  
-  this.show = function(){ 
-    fill(0,0,0);
-    ellipse(this.x,this.y,10,10);
-    
-    for (var i = 0; i < this.history.length; i++){
-      var pos = this.history[i];
-      ellipse(pos.x,pos.y,10,10);
+
+    this.show = function() {
+        fill(255, 255, 255,0);
+        ellipse(this.x, this.y, 1, 1);
+
+        for (var i = 0; i < this.history.length; i++) {
+            var pos = this.history[i];
+            ellipse(pos.x, pos.y, 10, 10);
+        }
+
+
     }
-    
-    
-  }
-  
+
 }
--- public/canvas/sketch.js
+++ public/canvas/sketch.js
@@ -3,32 +3,37 @@ var spark;
 markers = []
 
 function setup() {
-  createCanvas(600,400);
- 
-  lo = new logo();
-  markers.push(new marker(210,150,0,-1,260,150))
-  markers.push(new marker(210,150,.3,-1,260,150))
-  markers.push(new marker(276,150,-.3,-1,260,150))
-  markers.push(new marker(276,150,0,-1,260,150))
+    createCanvas(600, 400);
+
+    lo = new logo();
+    markers.push(new marker(210, 150, 0, -.5, 260, 150,1))
+    markers.push(new marker(210, 150, .15, -.3, 215 , 150,1))
+    markers.push(new marker(276, 150, -.15, -.3, 215, 150,1))
+    markers.push(new marker(276, 150, 0, -.5, 260, 150,1))
+    markers.push(new marker(276, 150, 0, -.5, 260, 150,1))
+    markers.push(new marker(384.5, 150, -1, -.2, 205, 150,1.036))
+    markers.push(new marker(381, 260, -1, .2, 280, 210,1.0356))
 
 
- 
+
+
 
 }
+
 function draw() {
- background(51, 205, 116);
- lo.fill();
- lo.strokeWeight();
- lo.show();
- lo.fadeInColor();
- 
- 
-  for (var i = 0 ; i <= markers.length ; i++){
-      if (i < markers.length){
-      var pos = markers[i]; 
-      pos.show();
-      pos.update();
-      }; 
-        
+    background(51, 205, 116);
+    lo.fill();
+    lo.strokeWeight();
+    lo.show();
+    lo.fadeInColor();
+
+
+    for (var i = 0; i <= markers.length; i++) {
+        if (i < markers.length) {
+            var pos = markers[i];
+            pos.show();
+            pos.update();
+        };
+
     };
-}; 
+};
